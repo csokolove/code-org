4c49d11b5bba251f00e5ad333bba9310150f9974
Charlie <94080789+csokolove@users.noreply.github.com>
2022-01-24 13:28:13 -0500

    Feat(parser): widen scope of use for the parser
    All properties of the URL() constructor are available to be used.
    
    mailto: URIs can be used with the open() function

--- src/bin/parser.js
+++ src/bin/parser.js
@@ -1,32 +1,65 @@
 /**
  * 
  * @param {URL} url - A stringified URL to be parsed
+ * @param {Boolean} mailto - A truthy value of whether the URI being opened is a mailto hyperlink
  * @param {String} property - Property of the parsed URL (hostname, pathname)
- * @returns Either the URL's hostname or pathname, depending on the property passed
+ * @returns The specified properties requested
  */
 
-const parser = (url, property) => {
+const parser = (url, mailto = false, ...property) => {
+    if(mailto == true) {
+        return url;
+    }
+    
     let pUrl;
     try {
         pUrl = new URL(url);
     } catch {
         throw new TypeError('The URL entered is not a valid URL.');
     }
-    if(typeof(property) !== 'string') throw new TypeError('URL parser requires a valid property in the form of a string.');
+    if(typeof(property) !== 'string') throw new TypeError('URL parser requires a valid parameter in the form of a string.');
 
     /*
-    Options - 
-    * https://dmitripavlutin.com/parse-url-javascript/
-    * hostname
-    * pathname
+    https://developer.mozilla.org/en-US/docs/Web/API/URL#properties
     */ 
-
-    if (property === 'hostname') {
-        return pUrl.hostname;
-    } else if (property === 'pathname') {
-        return pUrl.pathname;
-    } else {
-        throw new Error('Please enter a valid property');
+    
+    switch(hostname) {
+        case hash:
+            return pUrl.hash;
+            break;
+        case host:
+            return pUrl.host;
+            break;
+        case hostname:
+            return pUrl.hostname;
+            break;
+        case href:
+            return pUrl.href;
+            break;
+        case origin:
+            return pUrl.origin;
+            break;
+        case password:
+            return pUrl.password;
+            break;
+        case pathname:
+            return pUrl.pathname;
+            break;
+        case port:
+            return pUrl.port;
+            break;
+        case protocol:
+            return pUrl.protocol;
+            break;
+        case search:
+            return pUrl.search;
+            break;
+        case username:
+            return pUrl.username;
+            break;
+        default:
+            throw new Error('Please enter a valid property');
+            break;
     }
 }
 
